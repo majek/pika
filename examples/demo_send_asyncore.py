2fb9b0cfb421979284c2b1c40de7195ac4f730c2
majek <majek@debian.localdomain>
2009-11-25 05:37:37 -0500

    Send benchmarks

---
+++ examples/demo_send_asyncore.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import os
+import pika
+from pika.asyncore_adapter import add_oneshot_timer_rel
+import time
+
+host = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1'
+msgsize = (sys.argv[2]) if len(sys.argv) > 2 else 1024*1024*8
+credentials = pika.PlainCredentials('guest', 'guest')
+
+body = "a" * msgsize
+t0 = None
+ct = 20
+def send(conn, ch):
+    global t0, ct
+    if not conn.writable():
+        if t0 != None:
+            if not ct:
+                os.abort()
+            td = time.time() - t0
+            print "basic_publish took %.3fs"  % (td,)
+        t0 = time.time()
+        ch.basic_publish(exchange='',
+                        routing_key="test",
+                        body=body)
+        ct -= 1
+    add_oneshot_timer_rel(0.1, lambda:send(conn, ch))
+
+
+
+
+conn = pika.AsyncoreConnection(pika.ConnectionParameters(host,
+                               credentials=credentials))
+ch_prod = conn.channel()
+ch_prod.queue_declare(queue="test", durable=False, exclusive=False, auto_delete=True)
+add_oneshot_timer_rel(1.0, lambda :send(conn, ch_prod))
+
+
+while True:
+    pika.asyncore_loop()
